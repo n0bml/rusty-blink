554654db61970ae7db9e16061e585dda31f50cbe
Brendan Leber <n0bml@brendanleber.com>
2023-03-21 14:22:24 -0700

    Blink onboard and offboard LEDs.
    Added three LEDs and wires to a breadboard to expand the code handling more signals.
    
    The three LEDs and their connections are:
    
    - Onboard on **pin 13**.
    - Red on **pin 12**.
    - Green on **pin 11**.
    - Blue on **pin 10**.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Rusty Blink
 
-A small program to blink a LED.  Used to validate the tool chain to compile and
+A small program to blink some LEDs.  Used to validate the tool chain to compile and
 deploy [Rust](https://www.rust-lang.org/) code to an Arduino device.
 Specifically the [SparkFun](https://www.sparkfun.com/)
 [RedBoard](https://www.sparkfun.com/products/13975).
@@ -8,3 +8,7 @@ Specifically the [SparkFun](https://www.sparkfun.com/)
 Instead of cloning this repository please visit Rahix's
 [avr-hal](https://github.com/Rahix/avr-hal) repository on GitHub and follow the
 instructions there.
+
+## Picture Tax
+
+![RedBoard and Breadboard](./images/rust-blink.jpeg)
--- src/main.rs
+++ src/main.rs
@@ -7,19 +7,36 @@ use panic_halt as _;
 fn main() -> ! {
     let dp = arduino_hal::Peripherals::take().unwrap();
     let pins = arduino_hal::pins!(dp);
-    let mut led = pins.d13.into_output();
+
+    let mut onboard = pins.d13.into_output();
+    onboard.set_low();
+
+    let mut led_red = pins.d12.into_output();
+    led_red.set_high();
+
+    let mut led_green = pins.d11.into_output();
+    led_green.set_high();
+
+    let mut led_blue = pins.d10.into_output();
+    led_blue.set_high();
+
+    let mut counter: u64 = 0;
 
     loop {
-        led.set_high();
-        arduino_hal::delay_ms(1000);
+        counter += 1;
+        if (counter % 2) == 0 {
+            led_red.toggle();
+        }
 
-        led.set_low();
+        if (counter % 3) == 0 {
+            led_green.toggle();
+        }
+
+        if (counter % 4) == 0 {
+            led_blue.toggle();
+        }
+
+        onboard.toggle();
         arduino_hal::delay_ms(500);
-
-        led.set_high();
-        arduino_hal::delay_ms(1000);
-
-        led.set_low();
-        arduino_hal::delay_ms(2000);
     }
 }
